3eaed7aa397846ea3dba0ee56853e2c232f9c28f
unknown <navinsaurabh36@gmail.com>
2021-10-13 17:48:59 +0530

    Initial Commit

---
+++ springboot-backend/src/main/java/com/mindtree/springbootbackendangular/SpringbootBackendAngularApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree.springbootbackendangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendAngularApplication.class, args);
+	}
+
+}
